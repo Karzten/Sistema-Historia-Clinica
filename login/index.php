b6cf5dec4039c41ae04fcde45fb4a8d669e9157f
Luis Homero Zegarra Fernández <48776311+luisomero@users.noreply.github.com>
2021-05-13 00:37:06 -0500

    Apertura de sesión

--- login/index.php
+++ login/index.php
@@ -1,3 +1,9 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['S_user_id'])){
+		header('location: ../view/index.php');
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- view/index.php
+++ view/index.php
@@ -1,3 +1,9 @@
+<?php 
+	session_start();
+	if(!isset($_SESSION['S_user_id'])){
+		header('location: ../login/index.php');
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
