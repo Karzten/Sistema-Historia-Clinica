eff1efb57c871c1833d2986b05587cc8d5559efd
Luis Homero Zegarra Fernández <48776311+luisomero@users.noreply.github.com>
2021-05-14 15:30:04 -0500

    DataTables en español

--- view/index.php
+++ view/index.php
@@ -749,6 +749,34 @@
 <script src="../assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
+  var idioma_espanol = {
+    select: {
+      rows: "%d fila seleccionada"
+    },
+    "sProcessing":  "Procesando...",
+    "sLengthMenu":  "Mostrar _MENU_ registros",
+    "sZeroRecords": "No se encontraron resultados",
+    "sEmptyTable":  "Ningún dato disponible en esta tabla",
+    "sInfo":        "Registros del (_START_ al _END_) total de _TOTAL_ registros",
+    "sInfoEmpty":   "Registros del (0 al 0) total de 0 registros",
+    "sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sSearch":      "Buscar:",
+    "sUrl":         "",
+    "sInfoThousands": ",",
+    "sLoadingRecords": "<b>No se encontraron datos</b>",
+    "oPaginate": {
+      "sFirst":   "Primero",
+      "sLast":    "Último",
+      "sNext":    "Siguiente",
+      "sPrevious": "Anterior"
+    },
+    "oAria": {
+      "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+      "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    }
+  }
+
   function cargar_contenido(contenedor,contenido){
       $("#"+contenedor).load(contenido);
   }
